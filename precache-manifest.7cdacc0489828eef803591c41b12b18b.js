251287866b50f77048faef50d7814acc3b029c26
Amir Ben Artzi <abenartzi@gmail.com>
2019-11-19 18:36:59 +0200

    Updates

---
+++ precache-manifest.7cdacc0489828eef803591c41b12b18b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "542a93fb9efb7dd26d4a5ca9e1ccfb54",
+    "url": "/instagram-ui/index.html"
+  },
+  {
+    "revision": "0265d391705173c38751",
+    "url": "/instagram-ui/static/css/main.c390b6e9.chunk.css"
+  },
+  {
+    "revision": "3188e70bc6f25267f24f",
+    "url": "/instagram-ui/static/js/2.40e61ec9.chunk.js"
+  },
+  {
+    "revision": "0265d391705173c38751",
+    "url": "/instagram-ui/static/js/main.8047db16.chunk.js"
+  },
+  {
+    "revision": "fd329e52972bd349eb48",
+    "url": "/instagram-ui/static/js/runtime-main.9b17314b.js"
+  }
+]);
