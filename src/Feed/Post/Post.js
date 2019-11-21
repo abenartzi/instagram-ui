bc85b6955bc44fd472896a00303b32a50590fb12
abenartzi <abenartzi@gmail.com>
2019-11-21 13:34:23 +0200

    added like on/off toggle

--- src/Feed/Post/Post.js
+++ src/Feed/Post/Post.js
@@ -7,6 +7,23 @@ import {Link} from "react-router-dom";
 
 
 class Post extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            color : 'black',
+            like: false
+        }
+    }
+
+    heartSetColor = (e) => {
+        if(!this.state.like){
+            return this.setState({color:'red',like:true})
+        }else{
+            return this.setState({color:'black',like:false})
+        }
+    }
+
+    ;
 
     render() {
 
@@ -20,6 +37,7 @@ class Post extends Component {
                             <FontAwesomeIcon className={"calIcon"} icon={faCalendarAlt}/>
                             <span className="card-text">{this.props.created}</span>
                             </span>
+
                         <Link to={"/profile"} href={"#"} className="card-title"><FontAwesomeIcon className="userCircleIcon" icon={faUserCircle} size="2x"/></Link>
                     </div>
 
@@ -33,7 +51,7 @@ class Post extends Component {
 
                         <footer className={'cardFooter'}>
                             <TagList tags={this.props.tags}/>
-                            <div className="card-text"><FontAwesomeIcon className="heartIcon" icon={faHeart}/>{this.props.likes}</div>
+                            <div className="card-text" onClick={this.heartSetColor}><FontAwesomeIcon className="heartIcon" icon={faHeart} style={{color:this.state.color}}/>{this.props.likes}</div>
                         </footer>
 
                     </div>
