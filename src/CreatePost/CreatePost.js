3e0353097e363f309af4b160794540e47e28a6d7
abenartzi <abenartzi@gmail.com>
2019-11-26 21:23:44 +0200

    InputTag done

--- src/CreatePost/CreatePost.js
+++ src/CreatePost/CreatePost.js
@@ -4,7 +4,8 @@ import { Formik, Field, Form ,ErrorMessage} from 'formik';
 import './CreatePost.scss'
 import postModel from '../models/post.model'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {faCloudUploadAlt} from '@fortawesome/free-solid-svg-icons'
+import {faCloudUploadAlt, faShare, faShareAlt, faShareSquare} from '@fortawesome/free-solid-svg-icons'
+import InputTag from "./InputTag/InputTag";
 class CreatePost extends Component {
 
     submit(values) {
@@ -20,7 +21,7 @@ class CreatePost extends Component {
                     validationSchema={postModel} onSubmit={this.submit.bind(this)}>
                 <Form className="col-xs-12 col-sm-6">
                     <div className="form-group">
-
+                        {/*https://tympanus.net/codrops/2015/09/15/styling-customizing-file-inputs-smart-way/*/}
                         <Field className="inputfile" id="file" type="file" name="image"/>
                         <label htmlFor="file"> <FontAwesomeIcon icon={faCloudUploadAlt}/> <strong>Choose a file...</strong></label>
 
@@ -28,19 +29,20 @@ class CreatePost extends Component {
                         <ErrorMessage className="alert alert-danger" name="image" component="div" />
                         </div>
                     </div>
-                    <div className="form-group">
+                    <div className="row.form-group">
                         <label>Title:</label>
                         <Field as={"textarea"} name="title" className="form-control" placeholder="Enter Title..." spellCheck="false" maxLength={256}/>
                         <div className="container-fluid row">
                         <ErrorMessage className="alert alert-danger mt-2 my-2" name="title" component="div" />
                         </div>
                     </div>
-                    <div className="form-group">
+                    <div className="row.form-group">
                         <label>Tags:</label>
-                        <Field name="tags" className="form-control" placeholder="Enter Tags..."/>
+                        {/*<Field name="tags" className="form-control" placeholder="Enter Tags..."/>*/}
+                        <InputTag/>
                     </div>
-                    <div className="form-group">
-                        <Button type="submit">Share</Button> {/*Button from react bootstrap*/}
+                    <div className="row form-group d-flex justify-content-end">
+                        <Button type="submit" className={"shareBtn"}> <FontAwesomeIcon icon={faShare}/>Share</Button> {/*Button from react bootstrap*/}
 
                     </div>
                 </Form>
---
+++ src/CreatePost/InputTag/InputTag.js
@@ -0,0 +1,55 @@
+// https://medium.com/@jerrylowm/build-a-tags-input-react-component-from-scratch-1524f02acb9a
+
+import React, {Component} from 'react';
+import './InputTag.scss'
+class InputTag extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            tags:[ ]
+        }
+    }
+    inputKeyDown = (e) => {
+        const val = e.target.value;
+        if(e.key === 'Enter' && val){
+            if (this.state.tags.find(tag => tag.toLowerCase() === val.toLowerCase())) {
+                return;
+            }
+            e.preventDefault();
+            this.setState({tags:[...this.state.tags, val]});
+            this.tagInput.value = null;
+        }else if(e.key === 'Backspace' && !val) {
+            this.removeTag(this.state.tags.length -1);
+        }
+    };
+
+    removeTag = (i) => {
+        const newTags = [ ...this.state.tags ];
+        newTags.splice(i, 1);
+        this.setState({ tags: newTags });
+    };
+
+
+    render() {
+        return (
+            <div className="input-tag">
+                <ul className="input-tag_tags">
+
+                    {this.state.tags.map((tag,i) => (
+                        <li key={tag}>
+                            {tag}
+                            <button type="button" onClick={() => { this.removeTag(i); }}>+</button>
+                        </li>
+                    ))}
+                    <li className="input-tag_tags_input">
+                        <input type="text" onKeyDown={this.inputKeyDown} ref={c => {this.tagInput = c; }} placeholder={"Press enter to add tags"} maxLength={20}/>
+
+                    </li>
+
+                </ul>
+            </div>
+        );
+    }
+}
+
+export default InputTag;
